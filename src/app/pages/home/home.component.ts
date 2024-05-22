60cdc3c4268d23beec5fc574e6749ad611d44738
fabao1966 <fabao1966@yahoo.com.br>
2024-05-22 19:26:33 -0300

    implementando a função de cadastro

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, FormGroup, ReactiveFormsModule, Validators, FormControl } from '@angular/forms';
+import { Pessoa } from '../../models/Pessoa';
 
 @Component({
   selector: 'app-home',
@@ -17,11 +18,23 @@ export class HomeComponent {
   exibirTabela: boolean = false;
   btnCadastrar: boolean = true;
 
+  pessoas: Pessoa[] = [];
+
   formulario = new FormGroup({
     nome: new FormControl('', [Validators.required, Validators.minLength(3)]),
     idade: new FormControl(null, [Validators.required, Validators.min(0), Validators.max(120)]),
     cidade: new FormControl('', [Validators.required, Validators.minLength(3)])
 
   })
+
+  cadastrar(){
+    this.pessoas.push(this.formulario.value as Pessoa);
+
+    this.formulario.reset();
+
+    this.exibirTabela = true;
+
+    console.table(this.pessoas);
+  }
 
 }
