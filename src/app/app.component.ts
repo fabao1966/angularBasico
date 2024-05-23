889944de5511c521493cebac4debf3e5e7639067
fabao1966 <fabao1966@yahoo.com.br>
2024-05-23 10:57:49 -0300

    implementado modelo Produto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./pages/home/home.component";
+import { ProdutosComponent } from "./pages/produtos/produtos.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HomeComponent]
+    imports: [RouterOutlet, HomeComponent, ProdutosComponent]
 })
 export class AppComponent {
   title = 'angularprojeto';
