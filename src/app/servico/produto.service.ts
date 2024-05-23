ff10382f60d33eb9d3e3f7db62fbac3c761bd18e
fabao1966 <fabao1966@yahoo.com.br>
2024-05-23 11:57:47 -0300

    implementado CRUD em serviços de  produtos e criação da pasta enviroments

--- src/app/servico/produto.service.ts
+++ src/app/servico/produto.service.ts
@@ -1,9 +1,34 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Produto } from '../models/Produto';
+import { environment } from '../../environments/environment.development';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProdutoService {
 
-  constructor() { }
+export class ProdutoService {
+  //url:string = 'http://localhost:3000/produtos';
+  private url = environment.urlApi;
+
+  private http = inject(HttpClient);
+
+  selecionar():Observable<Produto[]>{
+    return this.http.get<Produto[]>(this.url);
+  }
+
+  cadastrar(obj:Produto):Observable<Produto>{
+    return this.http.post<Produto>(this.url, obj);
+  }
+
+  alterar(obj:Produto):Observable<Produto>{
+    return this.http.put<Produto>(`${this.url}/${obj.id}`, obj);
+  }
+
+  remover(id:number):Observable<any>{
+    return this.http.delete<any>(`${this.url}/${id}`);
+  }
+
 }
