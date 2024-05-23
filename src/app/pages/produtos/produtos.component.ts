53f12da2bcbea57789cf0e2f0f8c7395fedf5719
fabao1966 <fabao1966@yahoo.com.br>
2024-05-23 15:33:50 -0300

    implementado cadastro de produtos

--- src/app/pages/produtos/produtos.component.ts
+++ src/app/pages/produtos/produtos.component.ts
@@ -3,12 +3,14 @@ import { ProdutoService } from '../../servico/produto.service';
 import { Observable } from 'rxjs';
 import { Produto } from '../../models/Produto';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-produtos',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ],
   templateUrl: './produtos.component.html',
   styleUrl: './produtos.component.css'
@@ -17,15 +19,35 @@ export class ProdutosComponent {
 
   private produtoService = inject(ProdutoService);
 
-  produtos$ = new Observable<Produto[]>();
+  //produtos$ = new Observable<Produto[]>();
+  produtos: Produto[] = [];
   btnCadastrar: boolean = true;
 
   constructor(){
     this.selecionar();
   }
 
+  formulario = new FormGroup({
+    id: new FormControl(null),
+    nome: new FormControl(''),
+    valor: new FormControl(null)
+
+  })
+
   selecionar(){
-    this.produtos$ = this.produtoService.selecionar();
+    //this.produtos$ = this.produtoService.selecionar();
+    this.produtoService.selecionar()
+    .subscribe(result => { this.produtos = result });
+  }
+
+  cadastrar(){
+    this.produtoService
+    .cadastrar(this.formulario.value as Produto)
+    .subscribe(result =>{
+      this.produtos.push(result);
+
+      this.formulario.reset();
+    });
   }
 
 }
